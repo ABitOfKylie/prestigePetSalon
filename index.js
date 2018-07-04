0e6a1f451354d625ff88c092f985c03035336e5f
Kylie <kyliecalif@gmail.com>
2018-07-03 18:28:48 -0700

    multiple css fixes, added Service Choice cards - radiobtns with popovers

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// $('body').scrollspy({ target: '#navbarTogglerRef' })
+$('body').scrollspy({ target: '#navbarTogglerRef' });
+
+$(document).ready(function(){
+	$('[data-toggle="popover"]').popover({
+		placement:"top",
+		trigger:"hover"
+	});
+});
