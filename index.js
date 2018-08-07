edc0879388ba2c3af1d94654387fb2d8b16e5820
Kylie <kyliecalif@gmail.com>
2018-08-06 20:53:48 -0700

    button work, link checks, responsiveness recheck card/modal/carousel gallery in working order recheck focus/blue effect on See More Images button

--- index.js
+++ index.js
@@ -7,29 +7,42 @@ $(document).ready(function(){
 	});
 });
 
+//card flip listener
+var card = document.querySelector('.card');
+card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+});
+
+var addons = document.querySelector("#addOns");
+	addons.addEventListener('click', function() {
+		flipcard.classList.toggle("is-flipped");
+	});
+
+document.querySelector('#cardId1').classList.toggle('flip');
+// or using jQuery
+// $("#cardId").toggleClass("flip");
+
 //manual carousel controls
 // (function($){
 // 	"use strict";
-
-	$(".next").click(function(){
-		$(".carousel").carousel('next');
-		return false;
-	});
-	$(".prev").click(function(){
-		$(".carousel").carousel('prev');
-		return false;
-	});
+	// $(".next").click(function(){
+	// 	$(".carousel").carousel('next');
+	// 	return false;
+	// });
+	// $(".prev").click(function(){
+	// 	$(".carousel").carousel('prev');
+	// 	return false;
+	// });
 // })(jQuery);
 
 // pricing section - card flip
-document.querySelector('#cardId').classList.toggle('flip');
+// document.querySelector('#cardId').classList.toggle('flip');
 
 
 //appointment confirm remove button
 //initialize all tooltips-select by their data-toggle attribute
-
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-$('#deleteModal').modal(options);
+// $('#deleteModal').modal(options);
